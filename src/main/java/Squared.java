eee104b02e6e5042109c32f691ec456d7e356ba6
JuliansRecent <juliansrecent@gmail.com>
2023-02-05 18:24:31 -0800

    Module complete. Create a terminal based application that squares numbers given by the user.

--- src/main/java/Squared.java
+++ src/main/java/Squared.java
@@ -4,7 +4,13 @@ import java.util.Scanner;
 public class Squared {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
 
+        // Collect a number from the user
+        int numberToSquare = Integer.valueOf(reader.nextLine());
+        // Square the number
+        int result = numberToSquare * numberToSquare;
+        // Return the number
+        System.out.println(result);
     }
 }
